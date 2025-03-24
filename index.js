192aa20f0f9dfb67035da5098a7b868b4a8114e6
Carlos Sanchez <carlossanchez@Carloss-MacBook-Pro.local>
2025-03-23 22:18:55 -0400

    added custom domain to cors origin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const {
 // auth 1: restrict to only target origin
 app.use(cors());
 app.use(cors({
-    origin: process.env.CLIENT_URL, // Replace with your frontend domain
+    origin: [process.env.CLIENT_URL, 'https://www.swdetroitsupport.com/', 'https://swdetroitsupport.com/'], // Replace with your frontend domain
     // origin: '*', // * means any origin is accepted
     // methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     methods: ['GET'], // Allowed methods
